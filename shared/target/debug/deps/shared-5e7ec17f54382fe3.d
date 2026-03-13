4746f40b2bc69329f253aa8b6cd5efb999400f64
nuexq <nuexqq@gmail.com>
2026-03-13 22:14:42 +0000

    .gitignore

--- shared/target/debug/deps/shared-5e7ec17f54382fe3.d
+++
@@ -1,9 +0,0 @@
-/home/nuexq/Developer/nupu-8/shared/target/debug/deps/shared-5e7ec17f54382fe3.d: src/lib.rs Cargo.toml
-
-/home/nuexq/Developer/nupu-8/shared/target/debug/deps/libshared-5e7ec17f54382fe3.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
--- shared/target/debug/deps/shared-818b3c862a31c335.d
+++
@@ -1,9 +0,0 @@
-/home/nuexq/Developer/nupu-8/shared/target/debug/deps/shared-818b3c862a31c335.d: src/lib.rs Cargo.toml
-
-/home/nuexq/Developer/nupu-8/shared/target/debug/deps/libshared-818b3c862a31c335.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
