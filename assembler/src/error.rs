1fdbaf93315cacc10b262061a88b32ae4d2e7a09
nuexq <nuexqq@gmail.com>
2026-03-22 16:48:16 +0100

    refactor(error): remove the error prefix

--- assembler/src/error.rs
+++ assembler/src/error.rs
@@ -5,37 +5,30 @@ pub type Result<T> = std::result::Result<T, AssemblerError>;
 
 #[derive(Error, Debug)]
 pub enum AssemblerError {
-    #[error("{} line {line}: unknown opcode '{}'",
-        "error".red().bold(),
+    #[error("line {line}: unknown opcode '{}'",
         opcode.yellow()
     )]
     UnknownOpcode { line: usize, opcode: String },
 
-    #[error("{} line {line}: invalid register '{}'. must be r0-r15",
-        "error".red().bold(),
+    #[error("line {line}: invalid register '{}'. must be r0-r15",
         reg.yellow()
     )]
     InvalidRegister { line: usize, reg: String },
 
-    #[error("{} line {line}: label '{}' not found",
-        "error".red().bold(),
+    #[error("line {line}: label '{}' not found",
         label.yellow()
     )]
     UnknownLabel { line: usize, label: String },
 
-    #[error("{} line {line}: value '{}' is too large for 8 bits",
-        "error".red().bold(),
+    #[error("line {line}: value '{}' is too large for 8 bits",
         val.to_string().yellow()
     )]
     ValueOverflow { line: usize, val: u16 },
 
-    #[error("{} line {line}: missing argument for instruction",
-        "error".red().bold()
-    )]
+    #[error("line {line}: missing argument for instruction")]
     MissingArgument { line: usize },
 
-    #[error("{} line {line}: invalid number format '{}'",
-        "error".red().bold(),
+    #[error("line {line}: invalid number format '{}'",
         val.yellow()
     )]
     InvalidNumber { line: usize, val: String },
--- cpu/src/error.rs
+++ cpu/src/error.rs
@@ -6,9 +6,7 @@ pub type Result<T> = std::result::Result<T, CpuError>;
 
 #[derive(Error, Debug)]
 pub enum CpuError {
-    #[error("{} binary size ({actual}) exceeds memory capacity ({limit})",
-        "error:".red().bold(),
-    )]
+    #[error("binary size ({actual}) exceeds memory capacity ({limit})")]
     MemoryOverflow { actual: usize, limit: usize },
 
     #[error("{} {} jump to 0x{addr:02X} is out of memory bounds (max 0x{limit:02X})",
@@ -17,13 +15,9 @@ pub enum CpuError {
     )]
     InvalidJump { addr: usize, limit: usize },
 
-    #[error("{prefix} Instruction '{0:?}' is not a valid ALU operation", 
-        prefix = "error:".red().bold()
-    )]
+    #[error("Instruction '{0:?}' is not a valid ALU operation")]
     InvalidAluOperation(Instruction),
 
-    #[error("{prefix} instruction decoder: {0}", 
-        prefix = "error:".red().bold()
-    )]
+    #[error("instruction decoder: {0}")]
     Decode(#[from] shared::DecodeError),
 }
